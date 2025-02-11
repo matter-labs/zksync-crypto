999561edea30de4313beb549266170c58fd999f6
mm <mm@matterlabs.dev>
2025-02-11 14:49:49 +0100

    remove unused allocate_checked_with_tag, and warnings

--- crates/boojum/src/gadgets/tower_extension/fq12.rs
+++ crates/boojum/src/gadgets/tower_extension/fq12.rs
@@ -757,15 +757,6 @@ where
         Fq6::enforce_equal(cs, &a.c0, &b.c0);
         Fq6::enforce_equal(cs, &a.c1, &b.c1);
     }
-
-    fn allocate_checked_with_tag<CS: ConstraintSystem<F>>(
-        cs: &mut CS,
-        witness: T,
-        params: &Arc<Self::Params>,
-        place: crate::cs::Place,
-    ) -> Self {
-        todo!()
-    }
 }
 
 impl<F, NN> Selectable<F> for Fq12<F, BN256Fq, NN, BN256Extension12Params>
